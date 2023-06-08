e7e0b57e96dff1de19ea221c9e9c5672648d107e
LJH <95152341+LeeJinHyuk002@users.noreply.github.com>
2023-06-08 14:50:55 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.main_slide').slick({
+        vertical: true,
+        autoplay: true,
+        autoplaySpeebd: 5000,
+        arrows: false,
+        dots: true,
+    })
+})
+
